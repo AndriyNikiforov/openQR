9cdea5ca679e851bf07aeb4fa930d0c8df0d1749
AndriyNikiforov <andriy.nikiforov.general@gmail.com>
2020-03-16 11:58:22 +0200

    Add controller ProjectMember. Created validators ProjectMember add/remove.

---
+++ app/Controllers/Http/ProjectMemberController.js
@@ -0,0 +1,13 @@
+'use strict'
+
+class ProjectMemberController {
+  async add({ request, response }) {
+
+  }
+
+  async remove({ request, response }) {
+
+  }
+}
+
+module.exports = ProjectMemberController
---
+++ app/Validators/ProjectMemberRemove.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class ProjectMemberRemove {
+  get rules () {
+    return {
+      id: 'required|integer'
+    }
+  }
+}
+
+module.exports = ProjectMemberRemove
