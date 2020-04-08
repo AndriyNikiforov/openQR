f178a0de913003ae460c34f01ff5f913ac94ec65
AndriyNikiforov <andriy.nikiforov.general@gmail.com>
2020-04-08 12:35:04 +0300

    Created board column row methods.

--- app/Validators/BoardColumnRowCreate.js
+++ app/Validators/BoardColumnRowCreate.js
@@ -3,8 +3,31 @@
 class BoardColumnRowCreate {
   get rules () {
     return {
-      // validation rules
+      title: 'required|string',
+      description: 'required|string',
+      time_estimated: 'required|string',
+      time_tracked: 'string',
+      board_column_id: 'required|integer'
     }
+  }
+
+  get validateAll () {
+    return true;
+  }
+
+  get messages () {
+    return {
+      'title.required': 'You must provide a title',
+      'description.required': 'You must provide a description',
+      'time_estimated.required': 'You must provide a time estimated'
+    };
+  }
+
+  async fails (errorMessages) {
+    this.ctx.session.withErrors(errorMessages)
+      .flashAll()
+
+    return this.ctx.response.redirect('back');
   }
 }
 
--- app/Validators/BoardColumnRowUpdate.js
+++ app/Validators/BoardColumnRowUpdate.js
@@ -3,8 +3,32 @@
 class BoardColumnRowUpdate {
   get rules () {
     return {
-      // validation rules
+      title: 'required|string',
+      description: 'required|string',
+      time_estimated: 'required|string',
+      time_tracked: 'required|string',
+      board_column_id: 'required|integer'
     }
+  }
+
+  get validateAll () {
+    return true;
+  }
+
+  get messages () {
+    return {
+      'title.required': 'You must provide a title',
+      'description.required': 'You must provide a description',
+      'time_estimated.required': 'You must provide a time estimated',
+      'time_tracked.required': 'You must provide a time tracked'
+    };
+  }
+
+  async fails (errorMessages) {
+    this.ctx.session.withErrors(errorMessages)
+      .flashAll()
+
+    return this.ctx.response.redirect('back');
   }
 }
 
